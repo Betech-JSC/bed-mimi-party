549d99787fc7dda5fd82ac031096febf268e19c5
TrungThanh-Cao <dev.thanhcao@gmail.com>
2025-12-01 17:17:08 +0700

    Push code

--- resources/Frontend/js/Pages/Posts/Index.vue
+++ resources/Frontend/js/Pages/Posts/Index.vue
@@ -1,6 +1,6 @@
 <template>
     <main class="bg-primary-900">
-        <section class="relative z-10 h-[500px] xl:h-[720px]">
+        <section v-if="posts.data && posts.data.length > 0" class="relative z-10 h-[500px] xl:h-[720px]">
             <div class="absolute inset-0">
                 <JPicture
                     src="/assets/images/demo/image-blog-demo.jpg"
@@ -13,30 +13,41 @@
             <div class="relative pb-4 md:pb-5 xl:pb-6 h-full">
                 <div class="container flex items-end mt-auto h-full">
                     <div class="text-white space-y-4 xl:space-y-5">
-                        <h3 class="display-3 font-extrabold uppercase line-clamp-2">
-                            Tổ chức sự kiện được hiểu một cách đơn giản là quá trình lên ý tưởng sự kiện
-                        </h3>
+                        <Link
+                            :href="
+                                route('posts.show', {
+                                    slug: item.slug || 'demo-slug',
+                                })
+                            "
+                            class="display-3 font-extrabold uppercase line-clamp-2"
+                        >
+                            {{ posts.data[0].title }}
+                        </Link>
                         <div class="flex items-center gap-3">
                             <div class="title-2">Tên tác giả</div>
                             <div>|</div>
-                            <div class="body-1">20pm 20.8.2025</div>
+                            <div class="body-1">{{ formatDate(posts.data[0].published_at) }}</div>
                         </div>
                         <div class="title-2 !font-normal line-clamp-2 md:line-clamp-3">
-                            Gala dinner là một trong những sự kiện quan trọng được tổ chức để tri ân nhân viên, đối tác
-                            và khách hàng. Đây chính là cơ hội để mọi người có thời gian thực sự trò chuyện và gắn kết
-                            với nhau. Tuy nhiên, để một gala dinner thực sự nổi bật và để lại dấu ấn khó quên, bạn cần
-                            chuẩn bị những ý tưởng sáng tạo, độc đáo.
+                            {{ posts.data[0].description }}
                         </div>
-                        <button class="btn btn-primary">Xem thêm</button>
+                        <Link
+                            :href="
+                                route('posts.show', {
+                                    slug: posts.data[0]?.slug || 'demo-slug',
+                                })
+                            "
+                            class="btn btn-primary"
+                            >Xem thêm</Link
+                        >
                     </div>
                 </div>
             </div>
         </section>
-        <!-- {{ posts }} -->
         <section class="md:py-16 py-12 xl:py-20">
             <div class="container">
                 <div class="grid md:grid-cols-2 grid-cols-1 lg:grid-cols-3 md:gap-6 gap-4 xl:gap-8">
-                    <CardPost v-for="(itemPost, indexPost) in posts.data" :key="indexPost" :item="itemPost" />
+                    <CardPost v-for="(itemPost, indexPost) in posts.data.slice(1)" :key="indexPost" :item="itemPost" />
                 </div>
             </div>
         </section>
@@ -49,5 +60,13 @@ import CardPost from '@/Components/Card/CardPost.vue'
 export default {
     components: { CardPost },
     props: ['categories', 'posts', 'banner'],
+        methods: {
+        formatDate(dateString) {
+            // Convert '2025-09-05' to '05.09.2025'
+            if (!dateString) return ''
+            const [year, month, day] = dateString.split('-')
+            return `${day.padStart(2, '0')}-${month.padStart(2, '0')}-${year}`
+        },
+    }
 }
 </script>
