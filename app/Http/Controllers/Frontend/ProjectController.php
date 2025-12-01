8fa1696827fb884f04b3afda7e95820b20cec12d
Toan <nguyencongtoana1nhvt@gmail.com>
2025-12-01 11:22:03 +0700

    Update

---
+++ app/Http/Controllers/Frontend/ProjectController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Models\Post\Post;
+use Inertia\Inertia;
+use Illuminate\Routing\Controller;
+
+class ProjectController extends Controller
+{
+    public $model = Post::class;
+
+    public function index()
+    {
+        try {
+            $projects = Post::query()
+                ->active()
+                ->paginate(3)
+                ->onEachSide(0)
+                ->through(function ($item) {
+                    return $item->transform();
+                })
+                ->withQueryString();
+
+            $data = [
+                'projects' => $projects,
+            ];
+
+            if (request()->wantsJson()) {
+                return response()->json($data);
+            }
+
+            return Inertia::render('Projects/Index', $data);
+        } catch (\Throwable $th) {
+            dd($th);
+        }
+    }
+
+    public function show($slug) {}
+}
---
+++ resources/Backend/js/Pages/Projects/Index.vue
@@ -0,0 +1,11 @@
+<template layout>
+    <Table
+        :schema="schema"
+        :columns="['id', 'image', 'title', , 'is_featured', 'status', 'view_count', 'created_at', 'updated_at']"
+    />
+</template>
+<script>
+export default {
+    props: ['schema'],
+}
+</script>
