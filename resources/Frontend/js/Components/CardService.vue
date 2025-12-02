39f2d3d7f3500f1a14981bbe1a5d0749be401b3a
TrungThanh-Cao <dev.thanhcao@gmail.com>
2025-12-02 16:38:13 +0700

    Pussh code update hover serrvice

--- resources/Frontend/js/Components/CardService.vue
+++ resources/Frontend/js/Components/CardService.vue
@@ -1,5 +1,5 @@
 <template>
-    <article v-if="item.slug" class="flex relative flex-col justify-center items-start w-full">
+    <article v-if="item.slug" class="flex relative flex-col justify-center items-start w-full group">
         <div class="block relative overflow-hidden aspect-w-4 aspect-h-5 w-full h-full">
             <JPicture
                 :src="item.image.url || '/cover.jpg'"
@@ -7,6 +7,9 @@
                 wrapperClass="picture-cover"
                 class="w-full h-full object-cover lg:group-hover:scale-105 duration-300 ease-in-out"
             />
+            <div class="bg-overlay absolute inset-0 z-10 w-full h-full flex items-center justify-center opacity-0 lg:group-hover:opacity-100 duration-300 ease-in-out">
+                <button class="btn btn-primary min-w-[222px] justify-center">Xem chi tiết</button>
+            </div>
         </div>
         <div class="relative py-6 px-4 w-full">
             <div class="absolute inset-0 bg-linear w-full h-full"></div>
@@ -30,5 +33,8 @@ export default {
 <style lang="scss" scoped>
 .bg-linear {
     background: linear-gradient(91.96deg, #032665 0%, #01050e 100%);
+}
+.bg-overlay {
+    background: linear-gradient(180deg, rgba(3, 36, 95, 0) 0%, #03245F 100%);
 }
 </style>
