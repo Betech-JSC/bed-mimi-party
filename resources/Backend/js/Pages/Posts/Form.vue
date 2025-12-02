865ac6dc297561ccfd1c632e41ddcb2d7af40449
Toan <nguyencongtoana1nhvt@gmail.com>
2025-12-02 12:43:35 +0700

    fix images

--- resources/Backend/js/Pages/Posts/Form.vue
+++ resources/Backend/js/Pages/Posts/Form.vue
@@ -12,7 +12,7 @@
                             label: 'Tiêu đề',
                         }"
                     />
-                     <Field
+                    <Field
                         v-model="form.author"
                         :field="{
                             type: 'text',
@@ -76,16 +76,16 @@
                             label: 'Ảnh đại diện',
                             multiple: false,
                         }"
-                    /> 
-                        <Field
+                    />
+                    <Field
                         v-model="form.images"
                         :field="{
                             type: 'file_upload',
-                            name: 'image',
+                            name: 'images',
                             label: 'Danh sách ảnh',
-                            multiple: false,
+                            multiple: true,
                         }"
-                    /> 
+                    />
                     <Field
                         v-model="form.banners"
                         :field="{
@@ -140,7 +140,7 @@ export default {
                 images: this.item.images ?? [],
                 view_count: 0,
                 ...this.item,
-                 type: 'PROJECT',
+                type: 'PROJECT',
             }
         },
     },
