bd5ade1159fb43d5d0854fe37bc3f806b6795d52
TrungThanh-Cao <dev.thanhcao@gmail.com>
2025-12-03 19:53:08 +0700

    Push code

--- resources/Frontend/js/Components/SectionProject.vue
+++ resources/Frontend/js/Components/SectionProject.vue
@@ -2,8 +2,21 @@
     <section v-if="projects && projects.length > 0" class="md:py-16 py-12 xl:py-20">
         <div class="container space-y-6">
             <h2 class="title-linear display-2 uppercase font-extrabold text-center">Dự án</h2>
-            <div class="grid md:grid-cols-3 grid-cols-2 gap-y-4 gap-x-3 md:gap-y-5 md:gap-x-4">
-                <CardRoom v-for="(itemProject, indexProject) in projects" :key="indexProject" :item="itemProject" />
+            <div>
+                <div class="hidden md:grid grid-cols-3 gap-y-5 gap-x-4">
+                    <CardRoom
+                        v-for="(itemProject, indexProject) in projects.slice(0, 9)"
+                        :key="indexProject"
+                        :item="itemProject"
+                    />
+                </div>
+                <div class="grid grid-cols-2 gap-y-4 gap-x-3 md:hidden">
+                    <CardRoom
+                        v-for="(itemProject, indexProject) in projects"
+                        :key="indexProject"
+                        :item="itemProject"
+                    />
+                </div>
             </div>
         </div>
     </section>
